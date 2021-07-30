8837151c65e92204a0480ac64f41a5cbcebdc664
victorog17 <victor.og17@gmail.com>
2021-07-30 20:18:38 -0300

    commit 2021-07-30

--- api/handler.py
+++ api/handler.py
@@ -1,45 +1,45 @@
+import sys
+sys.path.append('C:/Users/Victor_1/repos/DataScience_Em_Producao/api/rossmann/')
 import pickle
 import pandas as pd
 from flask             import Flask, request, Response
-from rossmann.Rossmann import Rossmann
+from Rossmann import Rossmann
 
 # loading model
-model = pickle.load( open( '/Users/meigarom/repos/DataScience_Em_Producao/model/model_rossmann.pkl', 'rb') )
+model = pickle.load(open('C:/Users/Victor_1/repos/DataScience_Em_Producao/model/model_rossmann.pkl', 'rb'))
 
 # initialize API
-app = Flask( __name__ )
+app = Flask(__name__)
 
-@app.route( '/rossmann/predict', methods=['POST'] )
-def rossmann_predict():
+@app.route('/rossmann/predict', methods=['POST'])
+def rossman_predict():
     test_json = request.get_json()
-   
+    
     if test_json: # there is data
-        if isinstance( test_json, dict ): # unique example
-            test_raw = pd.DataFrame( test_json, index=[0] )
+        if isinstance(test_json, dict): # unique exemple
+            test_raw = pd.DataFrame(test_json, index=[0])
+        else: # multiple exemple
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
             
-        else: # multiple example
-            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
-            
-        # Instantiate Rossmann class
+        # Instantiate Rossmann Class
         pipeline = Rossmann()
         
         # data cleaning
-        df1 = pipeline.data_cleaning( test_raw )
+        df1 = pipeline.data_cleaning(test_raw)
         
         # feature engineering
-        df2 = pipeline.feature_engineering( df1 )
+        df2 = pipeline.feature_engineering(df1)
         
         # data preparation
-        df3 = pipeline.data_preparation( df2 )
+        df3 = pipeline.data_preparation(df2)
         
         # prediction
-        df_response = pipeline.get_prediction( model, test_raw, df3 )
+        df_response = pipeline.get_prediction(model, test_raw, df3)
         
         return df_response
         
-        
     else:
-        return Reponse( '{}', status=200, mimetype='application/json' )
-
+        return Response('{}', status=200, mimetype='application/json')
+    
 if __name__ == '__main__':
-    app.run( '0.0.0.0' )
+    app.run( host = "127.0.0.1", port= 5000, debug=True)
